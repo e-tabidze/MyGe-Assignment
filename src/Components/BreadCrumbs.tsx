dd088ddc6a964e7242b5ee42d0ba3f4c6f02f781
etabidze <etabidze@outlook.com>
2022-12-21 18:11:47 +0400

    chore: Create General Layout for Breadcrumbs

--- src/Components/BreadCrumbs.tsx
+++ src/Components/BreadCrumbs.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
-type Props = {}
+import React from "react";
+import { ArrowRight } from "../Assets/Icons/ArrowRight";
+type Props = {};
 
 export default function BreadCrumbs({}: Props) {
+  // TODO: Add Render Path with QueryString
+
   return (
-    <div>BreadCrumbs</div>
-  )
+    <div className="py-5 w-full flex flex-row items-center text-xs">
+      <span className="text-main-gray">მთავარი</span>
+      <ArrowRight className="mx-2" />
+      <span className="text-main-gray">ძიება</span>
+      <ArrowRight className="mx-2" />
+      <span className="text-main-orange">იყიდება</span>
+    </div>
+  );
 }
