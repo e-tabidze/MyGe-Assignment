211a0f7f75b4aa4e5fac5ff8d0898865cce91377
etabidze <etabidze@outlook.com>
2022-12-21 18:14:45 +0400

    feat: Import Header into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/Header';
 import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div>
+      <Header />
       <HomePage />
     </div>
   );
