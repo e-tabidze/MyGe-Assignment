28dd47c862cf0f14de338dae7893b9e1ff9d1b7b
etabidze <etabidze@outlook.com>
2022-12-21 18:15:59 +0400

    chore: Add Logo & Arrow SVG Components

---
+++ src/Assets/Icons/ArrowRight.tsx
@@ -0,0 +1,24 @@
+import { SVGProps } from "react";
+
+export const ArrowRight = ({
+  width = "5",
+  height = "8",
+  color,
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={width}
+    height={height}
+    {...props}
+    viewBox="0 0 5 8"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0.611328 1L3.61133 4L0.611328 7"
+      stroke={color || "#6F7383"}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
