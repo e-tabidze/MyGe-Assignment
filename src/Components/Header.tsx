96b49a9318d8bbcab7ba36c2df04965b3132007d
etabidze <etabidze@outlook.com>
2022-12-21 18:12:47 +0400

    feature: Create Finalized Layout for Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { LogoSVG } from "../Assets/Icons/Logo";
 
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <div>Header</div>
-  )
+    <div className="w-full p-[17px] bg-white-900">
+      <div className="max-w-[1050px] mx-auto">
+        <LogoSVG />
+      </div>
+    </div>
+  );
 }
