78104a116f0aa37d5280d882d763ac7c79038845
etabidze <etabidze@outlook.com>
2022-12-21 21:14:58 +0400

    refactor: Remove unnesessary code

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
 import BreadCrumbs from "../Components/BreadCrumbs";
 import Sidebar from "../Components/Sidebar";
-import {
-  getCategories,
-  getManufacturers,
-  getModels,
-  getProducts,
-} from "../Services/filter.service";
 
 type Props = {};
 
 export default function HomePage({}: Props) {
-  const [categories, setCategories] = useState([]);
-  const [models, setModels] = useState([]);
-  const [manufacturers, setManufacturers] = useState([]);
-
-  useEffect(() => {}, []);
-
   return (
     <div className="w-full h-screen bg-custom-gray">
       <div className="max-w-[1050px] mx-auto py-3">
