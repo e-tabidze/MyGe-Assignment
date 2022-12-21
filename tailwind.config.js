d215d907e8c346a4e0ef541d056f8230fa56479d
etabidze <etabidze@outlook.com>
2022-12-21 18:11:13 +0400

    chore: Add Custom Styling Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-gray': '#E5E5E5'
+      },
+      colors: {
+        'main-orange': '#FD4100',
+        'main-gray': '#6F7383',
+        'secondary-gray': '#8C929B',
+        'main-black': '#1B1D25',
+        'secondary-black': '#272A37',
+      }
+    },
   },
   plugins: [],
 }
