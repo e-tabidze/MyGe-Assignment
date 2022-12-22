5a0e6d5b692d9e76dca6d76b3e7aa7fe8c6c95d8
etabidze <etabidze@outlook.com>
2022-12-22 22:54:37 +0400

    feat: Add Basic Routing for HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css';
-import Header from './Components/Header';
-import HomePage from './Pages/HomePage';
+import "./App.css";
+import Header from "./Components/Header";
+import HomePage from "./Routes/HomePage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import loader from "./Helper/Redirect";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+    loader: loader,
+  },
+  {
+    path: "/ka/",
+    element: <HomePage />,
+  },
+]);
 
 function App() {
   return (
     <div>
       <Header />
-      <HomePage />
+      <RouterProvider router={router} />
     </div>
   );
 }
