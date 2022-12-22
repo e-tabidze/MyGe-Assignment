880aa4cb01be7f63cdf2640c3c86dabae3f75956
etabidze <etabidze@outlook.com>
2022-12-22 22:39:34 +0400

    style: Add BoxShadow Instance to Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       backgroundColor: {
-        'custom-gray': '#f2f3f6'
+        "custom-gray": "#f2f3f6",
+      },
+      boxShadow: {
+        "upper": "0px 2px 16px rgba(39, 42, 55, 0.13)",
       },
       colors: {
-        'main-orange': '#FD4100',
-        'main-gray': '#6F7383',
-        'secondary-gray': '#8C929B',
-        'main-black': '#1B1D25',
-        'secondary-black': '#272A37',
-      }
+        "main-orange": "#FD4100",
+        "main-gray": "#6F7383",
+        "secondary-gray": "#8C929B",
+        "main-black": "#1B1D25",
+        "secondary-black": "#272A37",
+      },
     },
   },
   plugins: [],
-}
+};
