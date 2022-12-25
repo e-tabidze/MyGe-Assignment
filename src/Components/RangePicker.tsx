72c9d9426c65c777355254bf8b2d3b3ca4ae2022
etabidze <etabidze@outlook.com>
2022-12-25 18:43:48 +0400

    feat: Added RangePicker Functionality

--- src/Components/RangePicker.tsx
+++ src/Components/RangePicker.tsx
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import CurrencySwitcher from "./CurrencySwitcher";
 
 type Props = {
   label: string;
   hasSwitcher?: boolean;
-  onChangeFrom?: VoidFunction;
-  onChangeTo?: VoidFunction;
+  fromName: string;
+  toName: string;
+  // onPriceChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default function RangePicker({
   label,
   hasSwitcher,
-  onChangeFrom,
-  onChangeTo,
-}: Props) {
+  fromName,
+  toName,
+}: // onPriceChange,
+Props) {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const [valueObj, setValueObj] = useState({
+    [fromName]: "",
+    [toName]: "",
+  });
+
+  let searchObj = Object.fromEntries(searchParams);
+
+  useEffect(() => {
+    handleSetInitValues();
+  }, [])
+
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if(value.length === 0) {
+      delete searchObj[name];
+    } else {
+      searchObj[name] = value;
+    }
+
+    setValueObj({
+      ...valueObj,
+      [name]: value
+    })
+    setSearchParams(searchObj);
+  };
+
+  const handleSetInitValues = () => {
+    let initState = {
+      [fromName]: searchObj[fromName] || "",
+      [toName]: searchObj[toName] || "",
+    }
+
+    console.log(initState, '[INIT VALUES]');
+    setValueObj(initState);
+  }
+
   return (
     <div className="">
       <div className="flex flex-row justify-between items-center">
@@ -24,17 +65,21 @@ export default function RangePicker({
       <div className="flex flex-row items-center justify-between mt-3 text-[13px]">
         <input
           className="w-[94px] h-10 border rounded-lg pl-[10px] outline-none"
+          onChange={handlePriceChange}
           placeholder="დან"
-          name="rangeFrom"
+          name={fromName}
           type="text"
+          value={valueObj[fromName]}
         />
         <div className="w-[6px] h-[2px] bg-secondary-gray rounded-full"></div>
-          <input
-            className="w-[94px] h-10 border rounded-lg pl-[10px] outline-none"
-            placeholder="მდე"
-            name="rangeTo"
-            type="text"
-          />
+        <input
+          className="w-[94px] h-10 border rounded-lg pl-[10px] outline-none"
+          onChange={handlePriceChange}
+          placeholder="მდე"
+          name={toName}
+          type="text"
+          value={valueObj[toName]}
+        />
       </div>
     </div>
   );
