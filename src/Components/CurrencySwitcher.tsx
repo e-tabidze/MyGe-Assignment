49d21ddf4fa86cc9b57886ccbf372dbebe928fd5
etabidze <etabidze@outlook.com>
2022-12-25 21:42:30 +0400

    feat: Add Dynamic Currency Change

--- src/Components/CurrencySwitcher.tsx
+++ src/Components/CurrencySwitcher.tsx
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import DollarSVG from "../Assets/Icons/DollarSign";
 import GelSVG from "../Assets/Icons/GelSign";
 
 type Props = {};
 
 export default function CurrencySwitcher({}: Props) {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [isGel, toggleIsGel] = useState(true);
 
+  let searchObj = Object.fromEntries(searchParams);
+  useEffect(() => {
+    const currID = searchObj.currId;
+
+    if (currID) {
+      toggleIsGel(JSON.parse(currID) === 3);
+    } else {
+      searchObj.currId = "3";
+      setSearchParams(searchObj);
+    }
+  }, [searchParams]);
+
+  const handleToggleGel = () => {
+    searchObj.currId = isGel ? "1" : "3";
+    setSearchParams(searchObj);
+  };
+
   return (
-    <div className="w-[46px] h-6 border relative rounded-full">
+    <div
+      className="w-[46px] h-6 border relative rounded-full"
+      onClick={handleToggleGel}
+    >
       <div
         className={`flex justify-center transition duration-150 cursor-pointer absolute top-[-1px] left-[-1px] items-center w-6 h-6 rounded-full ${
           isGel ? "bg-secondary-black" : "bg-transparent"
         }`}
-        onClick={() => toggleIsGel(true)}
       >
         <GelSVG color={isGel ? "#fff" : "#8C929B"} />
       </div>
@@ -21,7 +42,6 @@ export default function CurrencySwitcher({}: Props) {
         className={`flex justify-center transition duration-150 cursor-pointer absolute top-[-1px] right-[-1px] items-center w-6 h-6 rounded-full ${
           isGel ? "bg-transparent" : "bg-secondary-black"
         }`}
-        onClick={() => toggleIsGel(false)}
       >
         <DollarSVG color={isGel ? "#8C929B" : "#fff"} />
       </div>
